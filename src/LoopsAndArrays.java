4acdfa5deba70893c609290deac34f9daed2209b
Erick <erickgoncalves5@hotmail.com>
2023-02-19 01:56:51 -0300

    Complete a few methods of the exercise

--- src/LoopsAndArrays.java
+++ src/LoopsAndArrays.java
@@ -3,6 +3,34 @@ import java.util.Scanner;
 public class LoopsAndArrays {
     Scanner scan = new Scanner(System.in);
 
+    public void consonants() {
+        String[] consonantsArray = new String[6];
+        int consonantsAmount = 0;
+        int count = 0;
+
+        do {
+            System.out.println("letter: ");
+            String letter = scan.next();
+
+            if (!(letter.equalsIgnoreCase("a") |
+                letter.equalsIgnoreCase("e") |
+                letter.equalsIgnoreCase("i") |
+                letter.equalsIgnoreCase("o") |
+                letter.equalsIgnoreCase("u"))) {
+                consonantsArray[count] = letter;
+                consonantsAmount++;
+            }
+            count++;
+        } while ( count < consonantsArray.length);
+
+        System.out.println("Consonants: ");
+
+        for (String s : consonantsArray) {
+            if (s!=null)
+                System.out.print(s + " ");
+        }
+    }
+
     public void number() {
         System.out.println("Number: ");
         int number = scan.nextInt();
@@ -56,13 +84,47 @@ public class LoopsAndArrays {
         System.out.println("number of evens: " + evens);
     }
 
+    public void multiplicationTable() {
+        System.out.println("multiplication table: ");
+        int table = scan.nextInt();
+
+        System.out.println("table of " + table);
+
+        for (int i=1; i<=10; i++) {
+            System.out.println(table + "X" + i + " = " + (table*i));
+        }
+    }
+
+    public void factorial() {
+        System.out.println("factorial: ");
+        int factorial = scan.nextInt();
+        int multiplication = 1;
+        for (int i=factorial; i>=1; i--) {
+            multiplication = multiplication * i;
+        }
+        System.out.println(multiplication);
+    }
+
+    public void inverseOrder() {
+        int[] array = {0, -5, 15, 50, 8, 4};
+
+//        int count = 0;
+//        while (count < (array.length-1)) {
+//            System.out.print(array[count] + " ");
+//            count++;
+//        }
+
+//        for (int i : array) {
+//            System.out.println(i);
+//        }
+
+        for (int i = 0; i<array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
         LoopsAndArrays laa = new LoopsAndArrays();
-        laa.number();
-        laa.lineSeparator();
-        laa.biggestAndAverage();
-        laa.lineSeparator();
-        laa.oddAndEven();
-        laa.lineSeparator();
+        laa.consonants();
     }
 }
